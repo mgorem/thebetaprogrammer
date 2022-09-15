fa4ed429c5802b953df291b7edc97e7047d44799
callmemg <oremgitonga22@gmail.com>
2022-09-15 15:05:10 +0300

    client done, move to server node.js, express, mongodb

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {  
+    const footerStyle = {    
+        color: 'blue',    
+        fontStyle: 'italic',    
+        fontSize: 16  
+    }  
+    return (    
+    <div style={footerStyle}>      
+    <br />      
+    <em>Note app, by mgorem 2022</em>    
+    </div>  
+    )}
+
+    export default Footer
